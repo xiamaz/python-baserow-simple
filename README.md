93f4e174795cb7fb4ee6f3bd8f99353fa8e8f207
Max Zhao <max.zhao@arsbrevis.de>
2023-09-20 10:39:59 +0200

    Refactoring

--- README.md
+++ README.md
@@ -1,62 +1,13 @@
 
-# Python Project Template
+# python-baserow-simple
 
-A low dependency and really simple to start project template for Python Projects.
+This is a simple baserow API wrapper, that mainly does two things:
 
-See also 
-- [Flask-Project-Template](https://github.com/rochacbruno/flask-project-template/) for a full feature Flask project including database, API, admin interface, etc.
-- [FastAPI-Project-Template](https://github.com/rochacbruno/fastapi-project-template/) The base to start an openapi project featuring: SQLModel, Typer, FastAPI, JWT Token Auth, Interactive Shell, Management Commands.
-
-### HOW TO USE THIS TEMPLATE
-
-> **DO NOT FORK** this is meant to be used from **[Use this template](https://github.com/rochacbruno/python-project-template/generate)** feature.
-
-1. Click on **[Use this template](https://github.com/rochacbruno/python-project-template/generate)**
-3. Give a name to your project  
-   (e.g. `my_awesome_project` recommendation is to use all lowercase and underscores separation for repo names.)
-3. Wait until the first run of CI finishes  
-   (Github Actions will process the template and commit to your new repo)
-4. If you want [codecov](https://about.codecov.io/sign-up/) Reports and Automatic Release to [PyPI](https://pypi.org)  
-  On the new repository `settings->secrets` add your `PYPI_API_TOKEN` and `CODECOV_TOKEN` (get the tokens on respective websites)
-4. Read the file [CONTRIBUTING.md](CONTRIBUTING.md)
-5. Then clone your new project and happy coding!
-
-> **NOTE**: **WAIT** until first CI run on github actions before cloning your new project.
-
-### What is included on this template?
-
-- 🖼️ Templates for starting multiple application types:
-  * **Basic low dependency** Python program (default) [use this template](https://github.com/rochacbruno/python-project-template/generate)
-  * **Flask** with database, admin interface, restapi and authentication [use this template](https://github.com/rochacbruno/flask-project-template/generate).
-  **or Run `make init` after cloning to generate a new project based on a template.**
-- 📦 A basic [setup.py](setup.py) file to provide installation, packaging and distribution for your project.  
-  Template uses setuptools because it's the de-facto standard for Python packages, you can run `make switch-to-poetry` later if you want.
-- 🤖 A [Makefile](Makefile) with the most useful commands to install, test, lint, format and release your project.
-- 📃 Documentation structure using [mkdocs](http://www.mkdocs.org)
-- 💬 Auto generation of change log using **gitchangelog** to keep a HISTORY.md file automatically based on your commit history on every release.
-- 🐋 A simple [Containerfile](Containerfile) to build a container image for your project.  
-  `Containerfile` is a more open standard for building container images than Dockerfile, you can use buildah or docker with this file.
-- 🧪 Testing structure using [pytest](https://docs.pytest.org/en/latest/)
-- ✅ Code linting using [flake8](https://flake8.pycqa.org/en/latest/)
-- 📊 Code coverage reports using [codecov](https://about.codecov.io/sign-up/)
-- 🛳️ Automatic release to [PyPI](https://pypi.org) using [twine](https://twine.readthedocs.io/en/latest/) and github actions.
-- 🎯 Entry points to execute your program using `python -m <python_baserow_simple>` or `$ python_baserow_simple` with basic CLI argument parsing.
-- 🔄 Continuous integration using [Github Actions](.github/workflows/) with jobs to lint, test and release your project on Linux, Mac and Windows environments.
-
-> Curious about architectural decisions on this template? read [ABOUT_THIS_TEMPLATE.md](ABOUT_THIS_TEMPLATE.md)  
-> If you want to contribute to this template please open an [issue](https://github.com/rochacbruno/python-project-template/issues) or fork and send a PULL REQUEST.
-
-[❤️ Sponsor this project](https://github.com/sponsors/rochacbruno/)
-
-<!--  DELETE THE LINES ABOVE THIS AND WRITE YOUR PROJECT README BELOW -->
-
----
-# python_baserow_simple
+- return rows with column names and selections with their labels
+- easier batch-updates accepting both existing and new rows
 
 [![codecov](https://codecov.io/gh/xiamaz/python-baserow-simple/branch/main/graph/badge.svg?token=python-baserow-simple_token_here)](https://codecov.io/gh/xiamaz/python-baserow-simple)
 [![CI](https://github.com/xiamaz/python-baserow-simple/actions/workflows/main.yml/badge.svg)](https://github.com/xiamaz/python-baserow-simple/actions/workflows/main.yml)
-
-Awesome python_baserow_simple created by xiamaz
 
 ## Install it from PyPI
 
@@ -66,18 +17,38 @@ pip install python_baserow_simple
 
 ## Usage
 
+Import the library and provide an [Baserow API
+token](https://baserow.io/user-docs/personal-api-tokens) with sufficient privileges.
+
 ```py
-from python_baserow_simple import BaseClass
-from python_baserow_simple import base_function
+from python_baserow_simple import BaserowApi
 
-BaseClass().base_method()
-base_function()
-```
+api = BaserowApi(database_url="URL TO BASEROW DB", token="BASEROW TOKEN")
 
-```bash
-$ python -m python_baserow_simple
-#or
-$ python_baserow_simple
+# getting data
+table_data = api.get_data(table_id="<TABLE ID>")
+for entry_id, entry_data in table_data.items():
+    ...
+
+
+# updating entries for a table with a column named 'Name' accepting text
+data = {
+    "Name": "Hello World",
+}
+api.add_data("<TABLE ID>", data, row_id=<ROW ID>)
+
+# multiple entries can be updated at the same time
+entries = [
+    {
+        "id": None,  # this will create a new row
+        "Name": "AAA",
+    },
+    {
+        "id": 2,  # this will update row with baserow id 2
+        "Name": "BBB",
+    },
+]
+api.add_data_batch("<TABLE_ID>", entries)
 ```
 
 ## Development
--- python_baserow_simple/__init__.py
+++ python_baserow_simple/__init__.py
@@ -1,27 +1,58 @@
 from copy import deepcopy
-
 import requests
 
 
+def load_token(token_path):
+    with open(token_path) as tokenfile:
+        token = tokenfile.readline().strip()
+    return token
+
+
+def format_value(raw_value, field_info):
+    if field_info["type"] == "single_select":
+        if isinstance(raw_value, dict):
+            return raw_value["value"]
+        elif raw_value is None:
+            return raw_value
+        raise RuntimeError(f"malformed single_select {raw_value}")
+    elif field_info["type"] == "multiple_select":
+        if isinstance(raw_value, list):
+            return [v["value"] for v in raw_value]
+        raise RuntimeError(f"malformed multiple_select {raw_value}")
+    elif field_info["type"] == "link_row":
+        if isinstance(raw_value, list):
+            return [v["id"] for v in raw_value]
+        raise RuntimeError(f"malformed link_row {raw_value}")
+    else:
+        return raw_value
+
 class BaserowApi:
-    def __init__(self, token=None, token_path=None):
+    table_path = "api/database/rows/table"
+    fields_path = "api/database/fields/table"
+
+    def __init__(self, database_url: str, token=None, token_path=None):
+        self._database_url = database_url
         if token_path:
             self._token = load_token(token_path)
         if token:
             self._token = token
         self._fields = {}
 
-    def get_fields(self, table_id):
-        if table_id not in self._fields:
-            self._fields[table_id] = get_fields(table_id, self._token)
-        return self._fields[table_id]
+    def _get_fields(self, table_id):
+        get_fields_url = f"{self._database_url}/{self.fields_path}/{table_id}/"
+        resp = requests.get(
+            get_fields_url,
+            headers={"Authorization": f"Token {self._token}"},
+        )
 
-    def get_data(self, table_id):
-        return get_data(table_id, self._token)
+        resp.raise_for_status()
+        data = resp.json()
+        return data
 
     def _create_row(self, table_id, data):
+        create_row_url = f"{self._database_url}/{self.table_path}/{table_id}/?user_field_names=true"  # noqa: E501
         resp = requests.post(
-            f"https://phenotips.charite.de/api/database/rows/table/{table_id}/?user_field_names=true",  # noqa: E501
+            create_row_url,
             headers={
                 "Authorization": f"Token {self._token}",
                 "Content-Type": "application/json",
@@ -36,8 +67,9 @@ class BaserowApi:
             raise RuntimeError(f"Malformed response {resp_data}")
 
     def _update_row(self, table_id, row_id, data):
+        update_row_url = f"{self._database_url}/{self.table_path}/{table_id}/{row_id}/?user_field_names=true"  # noqa: E501
         resp = requests.patch(
-            f"https://phenotips.charite.de/api/database/rows/table/{table_id}/{row_id}/?user_field_names=true",  # noqa: E501
+            update_row_url,
             headers={
                 "Authorization": f"Token {self._token}",
                 "Content-Type": "application/json",
@@ -47,9 +79,9 @@ class BaserowApi:
         resp.raise_for_status()
 
     def _update_rows(self, table_id, datas):
-        url = f"https://phenotips.charite.de/api/database/rows/table/{table_id}/batch/?user_field_names=true"  # noqa: E501
+        update_rows_url = f"{self._database_url}/{self.table_path}/{table_id}/batch/?user_field_names=true"  # noqa: E501
         resp = requests.patch(
-            url,
+            update_rows_url,
             headers={
                 "Authorization": f"Token {self._token}",
                 "Content-Type": "application/json",
@@ -62,9 +94,9 @@ class BaserowApi:
         return ids
 
     def _create_rows(self, table_id, datas):
-        url = f"https://phenotips.charite.de/api/database/rows/table/{table_id}/batch/?user_field_names=true"  # noqa: E501
+        create_rows_url = f"{self._database_url}/{self.table_path}/{table_id}/batch/?user_field_names=true"  # noqa: E501
         resp = requests.post(
-            url,
+            create_rows_url,
             headers={
                 "Authorization": f"Token {self._token}",
                 "Content-Type": "application/json",
@@ -110,6 +142,55 @@ class BaserowApi:
                     data_conv[field["name"]] = new_value
         return data_conv
 
+    def _get_data(self, url):
+        resp = requests.get(url, headers={"Authorization": f"Token {self._token}"})
+        data = resp.json()
+
+        if "results" not in data:
+            raise RuntimeError(f"Could not get data from {url}")
+
+        if data["next"]:
+            return data["results"] + self._get_data(data["next"])
+        return data["results"]
+
+    def get_fields(self, table_id):
+        if table_id not in self._fields:
+            self._fields[table_id] = self._get_fields(table_id)
+        return self._fields[table_id]
+
+    def writable_fields(self, table_id):
+        fields = self.get_fields(table_id)
+        writable_fields = [
+            field for field in fields if not field["read_only"]
+        ]
+        return writable_fields
+
+    def get_data(self, table_id, writable_only = True):
+        """Get all data in a table.
+
+        writable_only - Only return fields which can be written to. This excludes all formula and computed fields.
+        """
+        if writable_only:
+            fields = self.writable_fields(table_id)
+        else:
+            fields = self.get_fields(table_id)
+        names = {f["name"]: f for f in fields}
+        get_data_url = f"{self._database_url}/{self.table_path}/{table_id}/?user_field_names=true"  # noqa: E501
+        data = self._get_data(
+            get_data_url
+        )
+
+        writable_data = {
+            d["id"]: {
+                k: format_value(v, names[k])
+                for k, v in d.items()
+                if k in names
+            }
+            for d in data
+        }
+
+        return writable_data
+
     def add_data(self, table_id, data, row_id=None) -> int:
         fields = self.get_fields(table_id)
         data_conv = self._convert_selects(data, fields)
@@ -134,79 +215,3 @@ class BaserowApi:
             self._create_rows(table_id, entries_new)
         if entries_update:
             self._update_rows(table_id, entries_update)
-
-
-def load_token(token_path):
-    with open(token_path) as tokenfile:
-        token = tokenfile.readline().strip()
-    return token
-
-
-def get_fields(table_id, token):
-    resp = requests.get(
-        f"https://phenotips.charite.de/api/database/fields/table/{table_id}/",
-        headers={"Authorization": f"Token {token}"},
-    )
-
-    resp.raise_for_status()
-    data = resp.json()
-    return data
-
-
-def get_writable_fields(table_id, token):
-    writable_fields = [
-        f for f in get_fields(table_id, token) if not f["read_only"]
-    ]
-    return writable_fields
-
-
-def _get_data(url, token):
-    resp = requests.get(url, headers={"Authorization": f"Token {token}"})
-    data = resp.json()
-
-    if "results" not in data:
-        raise RuntimeError
-
-    if data["next"]:
-        return data["results"] + _get_data(data["next"], token)
-    return data["results"]
-
-
-def format_value(raw_value, field_info):
-    if field_info["type"] == "single_select":
-        if isinstance(raw_value, dict):
-            return raw_value["value"]
-        elif raw_value is None:
-            return raw_value
-        raise RuntimeError(f"malformed single_select {raw_value}")
-    elif field_info["type"] == "multiple_select":
-        if isinstance(raw_value, list):
-            return [v["value"] for v in raw_value]
-        raise RuntimeError(f"malformed multiple_select {raw_value}")
-    elif field_info["type"] == "link_row":
-        if isinstance(raw_value, list):
-            return [v["id"] for v in raw_value]
-        raise RuntimeError(f"malformed link_row {raw_value}")
-    else:
-        return raw_value
-
-
-def get_data(table_id, token):
-    """Check a given table for empty keys."""
-    writable_fields = get_writable_fields(table_id, token)
-    writable_names = {f["name"]: f for f in writable_fields}
-    data = _get_data(
-        f"https://phenotips.charite.de/api/database/rows/table/{table_id}/?user_field_names=true",  # noqa: E501
-        token,
-    )
-
-    writable_data = {
-        d["id"]: {
-            k: format_value(v, writable_names[k])
-            for k, v in d.items()
-            if k in writable_names
-        }
-        for d in data
-    }
-
-    return writable_data
